cf2bdfbdf5aa8af0b171256946c29aed3a5f3ea2
SimonGP <simongoyanesmad@hotmail.com>
2017-05-19 12:49:03 +0200

    19-05

--- Rutas/php/clase_reservas.php
+++ Rutas/php/clase_reservas.php
@@ -45,7 +45,7 @@
 		}
 		//pagina web
 		function seleccionarTodasReservas($idUsuario){
-			$consulta="select reservas.ID,rutas.NOMBRE AS RUTAS,FECHA,PERSONAS from reservas join rutas ON rutas.ID = RESERVAS.ID_RUTA  WHERE ID_USUARIO = $idUsuario";
+			$consulta="select reservas.ID AS ID,rutas.NOMBRE AS RUTAS,FECHA,PERSONAS from reservas join rutas ON rutas.ID = RESERVAS.ID_RUTA  WHERE ID_USUARIO = $idUsuario";
 			if($resultado=$this->conexion->query($consulta)){
 				return $resultado;
 			}
