1c4c466eaa7ae09452fddac240957320a7fb820d
mariogg <fernandezmbf@gmail.com>
2017-05-20 21:02:36 +0200

    Cambios Mario
    Espero que no pete jajajaja

---
+++ Rutas/php/mostrar-guias.php
@@ -0,0 +1,36 @@
+<?php
+	require_once('clase_guias.php');
+    $datos = array();
+	class envio{
+		public $id="";
+		public $nombre_guia="";
+		public $experiencia="";
+		public $imagen_guia="";
+		
+		function __construct($id, $nombre_guia, $experiencia, $imagen_guia){
+			$this->id=$id;
+			$this->nombre_guia=$nombre_guia;
+			$this->experiencia=$experiencia;
+			$this->imagen_guia=$imagen_guia;
+			
+		}			
+	}
+	
+	
+	$enlace = new guias();
+
+if($resultado=$enlace->seleccionarTodosGuias()){
+			while($fila=$resultado->fetch_assoc()){	
+				$media=new envio($fila['ID'],$fila['NOMBRE_GUIA'],$fila['EXPERIENCIA'],$fila['IMAGEN_GUIA']);
+				array_push($datos, $media);
+			}
+		}
+
+if($datos!=""){
+		header('Content-Type: application/json');
+		echo json_encode($datos);
+	}
+	else{
+		echo 'No hay Datos';
+	}
+?>
